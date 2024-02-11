0eff87bac7606d0f2c0e1f5a663a846210b49fd5
kosaksongul <kosaksongul@gmail.com>
2024-02-11 22:09:27 +0300

    Dal

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using Intro.Emtities;
+﻿using Intro.DataAccess.Concretes;
+using Intro.Emtities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,37 +10,13 @@ namespace Intro.Business
 {
     public class CourseManager
     {
-        Course[] courses=new Course[3];
 
-        //constractor
-        public CourseManager()
+        public List<Course> GetAll()
         {
-            Course course1 = new Course();
-            course1.Id = 1;
-            course1.Name = "C#";
-            course1.Description = ".Net 8 vs...";
-            course1.Price = 0;
+            //business rules
+            CourseDal courseDal = new CourseDal();
 
-            Course course2 = new Course();
-            course2.Id = 2;
-            course2.Name = "JAVA";
-            course2.Description = "Java 17 ...";
-            course2.Price = 10;
-
-            Course course3 = new Course();
-            course3.Id = 3;
-            course3.Name = "Phyton";
-            course3.Description = "Phyton 1.12...";
-            course3.Price = 20;
-
-            courses[0] =course1;
-            courses[1] =course2;
-            courses[2] =course3;
-
-        }
-        public Course[] GetAll()
-        {
-            return courses;
+            return courseDal.GetAll();
         }
     }
 }
